00a171d2a1a2157e21121d4dabe5ba418313c973
HongMai27 <mtran3827@gmail.com>
2024-11-06 17:42:48 +0700

    new routes

--- API/src/routes/authRoute.ts
+++ API/src/routes/authRoute.ts
@@ -1,26 +1,39 @@
 import { Router } from "express";
-import { register, login, googleLogin } from "../controllers/authController";
-import { editProfile, followUser, getAllUser,  getFriendsWithStatus,  getUserFollowersAndFollowings, getUserProfile, suggestFriend, unfollowUser, updateStatus } from "../controllers/userController";
+import { register, login, googleLogin, forgotPassword } from "../controllers/authController";
+import { editProfile, followUser, getAllUser,  getFriendsWithStatus,  getUserFollowersAndFollowings, getUserProfile, searchUserByUsername, suggestFriend, unfollowUser, updateStatus, updateUserStatus } from "../controllers/userController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { getMess, sendMess } from "../controllers/messageController";
+import { getNotifications } from "../controllers/notificationController";
 
 const router = Router();
 
+//login, register, forgetpass, resetpass
 router.post("/register", register);
 router.post("/login", login);
 router.post('/google-login', googleLogin);
+router.post('/forgot-password', forgotPassword);
+
+//profile
 router.get('/', getAllUser)
 router.get('/user/:id', getUserProfile);
 router.put('/user/:id', authMiddleware, editProfile)
-router.post('/google-login', googleLogin);
+router.put("/update-status", authMiddleware, updateUserStatus);
+
+//follow, friend
 router.put('/follow/:id', authMiddleware, followUser);
 router.put('/unfollow/:id', authMiddleware, unfollowUser);
 router.put('/updatestatus', authMiddleware, updateStatus)
 router.get('/friend', authMiddleware, getFriendsWithStatus)
 router.get('/suggest', authMiddleware, suggestFriend)
 router.get('/getfollow', authMiddleware, getUserFollowersAndFollowings)
+
+//chat
 router.post('/:id/messages/:receiverId',authMiddleware, sendMess);
 router.get('/:id/messages/:receiverId', authMiddleware, getMess);
 
+//notifiaction
+router.get('/notifications/:id', authMiddleware, getNotifications)
 
+//search
+router.get('/users/search', searchUserByUsername); 
 export default router;
