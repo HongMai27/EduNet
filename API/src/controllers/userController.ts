00e36e71ce2768a4af40eea668ae8e481e386403
HongMai27 <mtran3827@gmail.com>
2024-12-01 19:53:12 +0700

    get user by id

--- API/src/controllers/userController.ts
+++ API/src/controllers/userController.ts
@@ -142,6 +142,25 @@ export const getUserProfile = async (req: Request, res: Response) => {
   }
 };
 
+//get user  by id
+export const getUserbyId = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+
+    const user = await User.findById(userId)
+      .select('-password -posts -savedPosts -followings -followers'); 
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 //follow user
 export const followUser = async (req: AuthRequest, res: Response) => {
   try {
