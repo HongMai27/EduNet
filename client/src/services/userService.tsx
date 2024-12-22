fd361af5da44efa2d953b61ed62a923472d4ac7f
HongMai27 <mtran3827@gmail.com>
2024-12-22 21:10:40 +0700

    userin4, sendstar

--- client/src/services/userService.tsx
+++ client/src/services/userService.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { IUser } from '../types/IUser'; 
+import { toast } from 'react-toastify';
 
 const API_URL = 'http://localhost:5000/api/auth';
 
@@ -7,6 +8,20 @@ const API_URL = 'http://localhost:5000/api/auth';
 export const fetchUserById = async (userId: string): Promise<IUser> => {
   const response = await axios.get<IUser>(`${API_URL}/user/${userId}`);
   return response.data;
+};
+
+//user in4
+export const fetchUserInfo = async (userId: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/userinfor/${userId}`);
+    return response.data; 
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const message = err.response?.data?.msg || 'Failed to fetch user information.';
+      throw new Error(message);
+    }
+    throw new Error('An unknown error occurred while fetching user information.');
+  }
 };
 
 //edit profile
@@ -132,4 +147,31 @@ export const searchUsersByUsername = async (searchTerm: string) => {
   }
 };
 
+export const sendStar = async (userId: string) => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      toast.error("You must be logged in to send star!");
+      return;
+    }
 
+    // Gọi API để cộng 3 điểm cho người dùng
+    const response = await axios.put(
+      `${API_URL}/user/${userId}/sendstar`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      toast.success("Star sent successfully! 3 points added.");
+    } else {
+      toast.error("Failed to send star.");
+    }
+  } catch (error) {
+    toast.error("Error sending star.");
+  }
+};
